fe47be766faa92f92808307ca29bdc1151d71a9c
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-01-17 14:55:25 -0300

    feature: WriteToPostGres

--- quotes-api/src/etl/transform.py
+++ quotes-api/src/etl/transform.py
@@ -15,52 +15,123 @@ from .logs import ConsoleInfo, ConsoleError, ConsoleWarning
 
 
 class TransformAPIData:
-    def __init__(self, FolderFiles) -> None:
+    def __init__(self, FolderFiles: str) -> None:
         self.path =  FolderFiles
         self.pipe_options = PipelineOptions(['--runner', 'Direct'])
-        self.postgres_conn =  {
+        self.connection =  {
             'host': 'localhost',
             'port': '5432',
             'database': 'DW',
             'user': 'SVC_DW',
             'password': 'SVC_DW'}
+        self.files = os.listdir(self.path)
         
-    def WriteToPostGres(self):
-        conn = {
-                'host': 'localhost',
-                'port': '5432',
-                'database': 'DW',
-                'user': 'SVC_DW',
-                'password': 'SVC_DW'}
+        self.CreateDynamicTable()
+            
+    def CreateDynamicTable(self):
+        """ 
+        SUMMARY: Create dynamic table from new extracted files.
+        """
+        files = self.files
+        ConsoleInfo("CreateDynamicTable: Start")
+        
+        try:                
+        # Conecte-se ao PostgreSQL
+            with psycopg2.connect(**self.connection) as conn:
+                # Abra um cursor para executar comandos SQL
+                with conn.cursor() as cursor:
+                    for file in files:
+                        file_schema = pq.read_schema(self.path + file, memory_map=True)
+                        schema_processing = [item + " text" for item in file_schema.names]
+                        table_schema = ', '.join(schema_processing)
+
+
+                        table_name = file.split("-")[0:2]
+                        table_name = '-'.join(table_name)
+                        query = f"""CREATE TABLE IF NOT EXISTS "STG"."{table_name}" ({table_schema + ", filename text, ts_execution timestamp with time zone"});"""
+                        
+                        cursor.execute(query)
+                        
+                        ConsoleInfo(f"CreateDynamicTable: Table Created: >>> 'STG'.{table_name}")
+        except Exception as err:
+            ConsoleError(f"CreateDynamicTable: >>> {err}")      
+        
+    def ListNewFiles(self):
+        """
+        SUMMARY: List new files from extracted files comparing with loaded files in database.
+        """
+        ConsoleInfo("Start Discovery new files for extract")
+        files = self.files 
+        tables =  [item.split("-")[0:2] for item in files]
+        tables = set(tuple(elemento) for elemento in tables)
                 
         # Conecte-se ao PostgreSQL
-        with psycopg2.connect(**conn) as conn:
+        with psycopg2.connect(**self.connection) as conn:
             # Abra um cursor para executar comandos SQL
             with conn.cursor() as cursor:
-                for file in os.listdir(self.path):
-                    file_schema = pq.read_schema(self.path + file, memory_map=True)
-                    schema_processing = [item + " text" for item in file_schema.names]
-                    table_schema = ', '.join(schema_processing)
-                
-                    table_name = '-'.join(file.split("-")[0:2])
-                    query = f"""CREATE TABLE IF NOT EXISTS "STG"."{table_name}" ({table_schema + ", filename text, ts_execution timestamp with timezone"});"""
+                for table in tables:
+                    table  = '-'.join(table)
+                    query = f"""
+                    SELECT distinct filename FROM "STG"."{table}"
+                    """
                     
                     cursor.execute(query)
                     
-                    query = f"""
-                        SELECT COUNT(1) FROM "STG"."{table_name}"
-                    """
+                    files_in_db = cursor.fetchall()
+                    files_in_db = [item[0] for item in files_in_db]
+                    
+                    # if len(files_in_db) >0: 
+                    NewFiles =  list(set(files) -  set(files_in_db)) 
+                    NewFiles = [self.path + item for item in NewFiles]
+                    ConsoleInfo(f"ListNewFiles: Files for Extract: >>> {[print(item) for item in NewFiles]}")
+
+                    return NewFiles
+                
+    class WriteToPostGress(beam.DoFn): 
+        def __init__(self, postgres_config):
+            self.conn = postgres_config
+            
+        
+        def process(self, element):
+            ConsoleInfo("InsertNewFileToPostgres: Start")
+            try:
+                # Conecte-se ao PostgreSQL
+                with psycopg2.connect(**self.conn) as conn:
+                    # Abra um cursor para executar comandos SQL
+                    with conn.cursor() as cursor:
+                        columns = ', '.join(element.column_names)
+                        placeholders = ', '.join(['%s'] * len(element))
+                        
+                        query = f""" 
+                            INSERT INTO nome_tabela ({columns})
+                            VALUES ({placeholders})
+                        """
+                        
+                        ## BREAK: Adicionar a lógica que lê os dados do parquet com um loop e monta a string da query dinamicamente.
+                        values = tuple(element.values())
+                        
+                        cursor.execute(query, values)
+                        ConsoleInfo("InsertNewFileToPostgres: Done")
+            except Exception as err:
+                ConsoleError(f"InsertNewFileToPostgres Error >>> {err}")
+                    
         
     def PipelineRun(self):
+        NewFilesForExtract = self.ListNewFiles()
         ConsoleInfo("Starting pipeline")
-        try:
-            with beam.Pipeline(options=self.pipe_options) as p:
-                beam_pipe = (
-                    p 
-                    | "ReadFromParquetFile" >> beam.io.ReadFromParquetBatched(self.path)
-                    | "Parquet to Pandas" >> beam.Map(lambda x: x.to_pandas())
-                    | "Printing DataFrame" >> beam.Map(print)
-                )
-            ConsoleInfo("Finished pipeline")
-        except Exception as err:
-            ConsoleError(f"Pipeline error: {err}")
+        for file in NewFilesForExtract:
+            ConsoleInfo(f"Reading >>> {file}")
+            try:
+                with beam.Pipeline(options=self.pipe_options) as p:
+                    beam_pipe = (
+                        p 
+                        | "ReadParquet" >> beam.io.ReadFromParquetBatched(file_pattern=file)
+                        | "ProcessFile" >> beam.ParDo(self.WriteToPostGress(self.connection))
+                        # | "ReadFromParquetFile" >> beam.io.ReadFromParquetBatched(self.path)
+                        # | "Parquet to Pandas" >> beam.Map(lambda x: x.to_pandas())
+                        # | "ProcessarDados" >> beam.Map(processar_dados)
+                        # | "Printing DataFrame" >> beam.Map(lambda x: print(x.to_pandas()))
+                    )
+                ConsoleInfo(f"Writed >>>> {file}")
+            except Exception as err:
+                ConsoleError(f"Pipeline error: {err}")
--- quotes-api/src/main.py
+++ quotes-api/src/main.py
@@ -2,11 +2,16 @@ from etl.extract import ExtractDataAPI
 from etl.transform import TransformAPIData
 import os
 
-api = "https://economia.awesomeapi.com.br/last/USD-BRL,ETH-USD,BTC-USD,USDL-BRL"
-path = f"/Users/{os.getenv('USER')}/repos/data-engineer-portfolio/API-ETL/apache-beam/awesome_data_ingestion/data/raw/"
+api = "https://economia.awesomeapi.com.br/last/BTC-USD,ETH-USD,USD-BRL"
+path = "/Users/ivsouza/repos/github.com/IvanildoBarauna/ETL-awesome-api/quotes-api/data/raw/"
 
 # NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
 # NewExtract.PipelineRun()
 
 NewTransform = TransformAPIData(FolderFiles=path)
 NewTransform.PipelineRun()
+
+# files = os.listdir(path)
+
+# for file in files:
+#     print(file)
