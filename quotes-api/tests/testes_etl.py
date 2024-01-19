29608d862f2868e2428ed56800d0f4ff04d357c4
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-01-19 09:14:10 -0300

    feat: saved last modifications

--- quotes-api/tests/testes_etl.py
+++ quotes-api/tests/testes_etl.py
@@ -1,3 +1,2 @@
 file = '/Users/ivsouza/repos/github.com/IvanildoBarauna/ETL-awesome-api/quotes-api/data/raw/BTC-USD-1705499505-00000-of-00001.parquet'
-
-new_file[len(new_file) -1:]
+file[len(file) -1:]
