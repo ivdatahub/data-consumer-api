6ab8864f96329731df3018d5377b980c0266ac4b
Ivanildo Barauna de Souza Junior (Pessoal) <ivanildo.jnr@outlook.com>
2024-03-24 18:45:38 -0300

    reorganized

--- app/main.py
+++ app/main.py
@@ -1,17 +1,9 @@
-from etl.extract import ExtractDataAPI
-from etl.transform import TransformAPIData
+from .extract import ExtractDataAPI
+from .transform import TransformAPIData
 import os
 
 api = "https://economia.awesomeapi.com.br/last/BTC-USD,ETH-USD,USD-BRL"
 path = "/Users/ivsouza/repos/github.com/IvanildoBarauna/ETL-awesome-api/quotes-api/data/raw/"
 
-# NewExtract = ExtractDataAPI(endpoint=api, output_path=path)
-# NewExtract.PipelineRun()
-
 NewTransform = TransformAPIData(FolderFiles=path)
 NewTransform.PipelineRun()
-
-# files = os.listdir(path)
-
-# for file in files:
-#     print(file)
