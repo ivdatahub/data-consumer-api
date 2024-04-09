485369f39d236ac8687760de5924192ef0dccdb5
Ivanildo Barauna de Souza Junior <ivanildo.souza@mercadolivre.com>
2024-04-09 17:40:40 -0300

    feat: main refact for call extract

--- etl/main.py
+++ etl/main.py
@@ -3,3 +3,12 @@ import os
 
 SCRIPT_DIR = os.path.dirname(os.path.abspath(__file__))
 sys.path.append(os.path.dirname(SCRIPT_DIR))
+
+from etl.jobs.extract.ApiToParquetFile import extraction
+
+def main():
+    extraction("USD-BRL", "BTC-BRL")
+
+if __name__ == "__main__":
+    main()
+    
