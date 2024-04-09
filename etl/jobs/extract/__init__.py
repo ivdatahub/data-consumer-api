0fc5f84ed2e9d4222a482c3845a06a7bbbafaa5e
Ivanildo Barauna de Souza Junior <ivanildo.souza@mercadolivre.com>
2024-04-09 17:41:23 -0300

    feat: added constants for clean coding

--- etl/jobs/extract/__init__.py
+++ etl/jobs/extract/__init__.py
@@ -3,5 +3,6 @@ import pyarrow
 import requests
 # Custom Logs
 from etl.utils.logs import ConsoleInfo, ConsoleError, ConsoleWarning
-from etl.utils.common import datetime, DefaultTimestampStr, DefaultOutputFolder
+from etl.utils.common import DefaultTimestampStr, DefaultOutputFolder
 from etl.config.beam import CustomBeam
+from etl.utils.constants import ENDPOINT_LIST_AVALIABLE_PARITYS, ENDPOINT_QUOTES_AWESOME_API
