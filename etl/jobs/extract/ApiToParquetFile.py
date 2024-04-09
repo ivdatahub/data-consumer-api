0f5e2448831bff9f4ae491a256e36196a77fdbfb
Ivanildo Barauna de Souza Junior <ivanildo.souza@mercadolivre.com>
2024-04-09 17:43:28 -0300

    feat: refact APIToParquet package

--- etl/jobs/extract/ApiToParquetFile.py
+++ etl/jobs/extract/ApiToParquetFile.py
@@ -1,117 +1,88 @@
 from etl.jobs.extract import (
     pyarrow, requests, ConsoleInfo, ConsoleError, ConsoleWarning
-    ,datetime, DefaultOutputFolder, DefaultTimestampStr, CustomBeam
+     ,DefaultOutputFolder, DefaultTimestampStr, CustomBeam
+    ,ENDPOINT_LIST_AVALIABLE_PARITYS, ENDPOINT_QUOTES_AWESOME_API
 )
 
 class extraction: 
-
-    def __init__(self, endpoint: str) -> None:
-        self.ExecutePipeline(uri=endpoint)
+    def __init__(self, *xargs: str) -> None:
+        self.params = xargs
+        self.PipelineRun()
+        
+    def APIToDicionary(self):
+        valParams = []
+        list_of_avaliable = requests.get(ENDPOINT_LIST_AVALIABLE_PARITYS).json()
+        
+        for param in self.params:
+            if param in list_of_avaliable:
+                valParams.append(param)
+            else:
+                ConsoleWarning(f"Param: {param} is not valid for call")
     
-    class ExecutePipeline():
-        def __init__(self, uri: str) -> None:
+        if valParams:
+            ConsoleInfo(f"Parameters OK >>> {valParams}")
+            response = requests.get(ENDPOINT_QUOTES_AWESOME_API + ','.join(valParams))
+            if response.ok:
+                ConsoleInfo(f"Response OK >>> {response}")
+                return dict(responseData=response.json(), params=valParams)
+            else:
+                ConsoleError(f"Response failed >>> {response}")
+
+    def ParquetSchemaLoad(self, element: dict):
+        try:
+            api_header = list(element.keys())
+            schema = []
+
+            for field in api_header:
+                schema += [(field, pyarrow.string())]
+
+            beam_schema = pyarrow.schema(schema)
+            ConsoleInfo("Schema - 200 OK")
+
+            return beam_schema
+
+        except Exception as Err:
+            ConsoleInfo(f"Schema - Error >>>> {Err}")
+
+    def PipelineRun(self):
+        response = self.APIToDicionary()
+        if response:
+            json_data = response["responseData"]
+            params = response["params"]
             
-            self.endpoint = uri
-            self.ExtractedFilePath = []
-            self.ValidParams = []
-            self.PipelineRun()
+            ## For generate schema is necessary extract one currency from dicionary
+            extract_index_params = [item.replace("-", "") for item in params]      
             
-        def APIToDicionary(self):
-            from extract import requests
-            def ValidListOfParams():
-                arr_endpoint = self.endpoint.split("/")
-                params = arr_endpoint[len(arr_endpoint) - 1]
-                lstParams = params.split(",")
-                
-                list_of_avaliable = requests.get("https://economia.awesomeapi.com.br/json/available").json()
-                
-                valParams = []
-                
-                for param in lstParams:
-                    if param in list_of_avaliable:
-                        valParams.append(param)
-                    else:
-                        ConsoleWarning(f"Param: {param} is not valid for call")
-                
-                self.endpoint = self.endpoint.replace(''.join(params), ','.join(valParams))
-                return valParams
-            
-            ParamsValidate = ValidListOfParams()
-            if ParamsValidate:
-                ConsoleInfo(f"Parameters OK >>> {ParamsValidate}")
-                response = requests.get(self.endpoint)
-                if response.ok:
-                    ConsoleInfo(f"Response OK >>> {response}")
-                    return dict(responseData=response.json(), params=ParamsValidate)
-                else:
-                    ConsoleError(f"Response failed >>> {response}")
+            FileSchema = self.ParquetSchemaLoad(json_data[extract_index_params[0]])
 
-        def CurrentTimestampStr(self) -> str:
-            from extract import datetime
-            current = datetime.now().timestamp()
-            return str(int(current))
+            for index, param in enumerate(params):
+                dic = json_data[param.replace("-", "")]
 
-        def ParquetSchemaLoad(self, element: dict):
-            from extract import pyarrow
-            try:
-                api_header = list(element.keys())
-                schema = []
-
-                for field in api_header:
-                    schema += [(field, pyarrow.string())]
-
-                beam_schema = pyarrow.schema(schema)
-
-                ConsoleInfo("Schema - 200 OK")
-
-                return beam_schema
-
-            except Exception as Err:
-                ConsoleInfo(f"Schema - Error >>>> {Err}")
-
-        def PipelineRun(self):
-            from extract import beam
-            response = self.APIToDicionary()
-            if response:
-                json_data = response["responseData"]
-                params = response["params"]
-                
-                ## For generate schema is necessary extract one currency from dicionary
-                extract_index_params = [item.replace("-", "") for item in params]      
-                
-                FileSchema = self.ParquetSchemaLoad(json_data[extract_index_params[0]]
-                                                    )
-                insert_date = self.CurrentTimestampStr()
-
-                for index, param in enumerate(params):
-                    dic = json_data[param.replace("-", "")]
-
-                    if dic:
-                        try:
-                            ConsoleInfo(
-                                f"Starting pipeline {index + 1} of {len(params)} - {param} - Starting!"
-                            )
-                            with CustomBeam.PipelineDirectRunner() as pipe:
-                                input_pcollection = (
-                                    pipe
-                                    | "Create" >> CustomBeam.BeamObj().Create([dic])
-                                    | "WriteToParquet"
-                                    >> CustomBeam.BeamObj().io.WriteToParquet(
-                                        file_path_prefix=f"{self.output_path}{param}-{insert_date}",
-                                        file_name_suffix=".parquet",
-                                        num_shards=1,
-                                        schema=FileSchema,
-                                    )
+                if dic:
+                    output_path = DefaultOutputFolder()
+                    insert_timestamp = DefaultTimestampStr()
+                    beam = CustomBeam.BeamObj()
+                    extracted_files = []
+                    try:
+                        ConsoleInfo(f"Starting pipeline {index + 1} of {len(params)} - {param} - Starting!")
+                        
+                        with CustomBeam.PipelineDirectRunner() as pipe:
+                            input_pcollection = (
+                                pipe
+                                | "Create" >> beam.Create([dic])
+                                | "WriteToParquet"
+                                >> beam.io.WriteToParquet(
+                                    file_path_prefix=f"{output_path}{param}-{insert_timestamp}",
+                                    file_name_suffix=".parquet",
+                                    num_shards=1,
+                                    schema=FileSchema,
                                 )
-
-                            ConsoleInfo(
-                                f"Pipeline execution OK >> {index + 1} of {len(params)} - {param} - Extracted!"
                             )
 
-                            self.ExtractedFilePath.append(
-                                f"{self.output_path}{param}-{insert_date}-00000-of-00001.parquet"
-                            )
-                            
+                        ConsoleInfo(f"Pipeline execution OK >> {index + 1} of {len(params)} - {param} - Extracted!")
 
-                        except Exception as err:
-                            ConsoleError(f"{param} - Pipeline Execution Error >>>  {err}")
+                        extracted_files.append(f"{output_path}{param}-{insert_timestamp}-00000-of-00001.parquet")
+                    
+                    except Exception as err:
+                        ConsoleError(f"{param} - Pipeline Execution Error >>>  {err}")
+                        
