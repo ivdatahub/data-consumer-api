44222bfc5627b0ee7b7fb0c417a9d98d4b5aca83
ivbarauna@macbook <ivanildo.jnr@outlook.com>
2024-04-13 00:46:51 -0300

    feat: added path to search

--- etl/jobs/extract/__init__.py
+++ etl/jobs/extract/__init__.py
@@ -2,7 +2,12 @@
 import pyarrow
 import requests
 # Custom Logs
-from etl.utils.logs import ConsoleInfo, ConsoleError, ConsoleWarning
+from etl.utils.logs import loggingInfo, loggingError, loggingWarn
 from etl.utils.common import DefaultTimestampStr, DefaultOutputFolder
 from etl.config.beam import CustomBeam
 from etl.utils.constants import ENDPOINT_LIST_AVALIABLE_PARITYS, ENDPOINT_QUOTES_AWESOME_API
+import os
+
+current_dir = os.path.dirname(os.path.relpath(__file__))
+WORK_DIR = current_dir.split("/")[-1:][0]
+
