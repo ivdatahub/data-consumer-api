0ed37686db2b8fa9d75dea89e343c6649b78ca3d
ivbarauna@macbook <ivanildo.jnr@outlook.com>
2024-04-13 00:48:40 -0300

    feat: extraction endpoint

--- etl/__init__.py
+++ etl/__init__.py
@@ -1,13 +1,8 @@
 from etl.jobs.extract.ApiToParquetFile import extraction
-# next steps
-    # from etl.jobs.transform import transformation
-    # from etl.jobs.extract.load import loadData
+# from etl.jobs.load.ParquetToDataBase import pgLoading
 
 class ExecutePipeline:
     def __init__(self, *xargs) -> None:
         extraction(xargs)
+        # pgLoading()
         
-        # next steps
-            # transformation()
-            # loadData()
-            
