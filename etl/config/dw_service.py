8ebc201226d2f3391475f7a6cd5986703f736ca1
ivbarauna@macbook <ivanildo.jnr@outlook.com>
2024-04-13 00:46:28 -0300

    feat: dw_service configuration

--- etl/config/dw_service.py
+++ etl/config/dw_service.py
@@ -4,27 +4,29 @@ import psycopg2
 
 class NewDBConnection:
     def __init__(self) -> None:
-        self.conn =  GetConnectionsParameters()
+        self.conn =  self.GetConnectionsParameters
         
-        def GetConnectionsParameters() -> dict:
-            with open('docker-compose.yaml', 'r') as DockerComposeFile:
-                FileContent = yaml.safe_load(DockerComposeFile)
-                dwService = FileContent["services"]["dw-service"]
-                dbEnvParams = dwService["environment"]
-                
-            PORT = dwService["ports"][0].split(":")[0]
-            USER = dbEnvParams["POSTGRES_USER"]
-            PASS = dbEnvParams["POSTGRES_PASSWORD"]
-            DB_NAME = dbEnvParams["POSTGRES_DB"]
+    def GetConnectionsParameters() -> dict:
+        with open('docker-compose.yaml', 'r') as DockerComposeFile:
+            FileContent = yaml.safe_load(DockerComposeFile)
+            dwService = FileContent["services"]["dw-service"]
+            dbEnvParams = dwService["environment"]
             
-            return  {
-                'host': 'localhost',
-                'port': PORT,
-                'database': DB_NAME,
-                'user': USER,
-                'password': PASS
-                }
+        PORT = dwService["ports"][0].split(":")[0]
+        USER = dbEnvParams["POSTGRES_USER"]
+        PASS = dbEnvParams["POSTGRES_PASSWORD"]
+        DB_NAME = dbEnvParams["POSTGRES_DB"]
+        
+        return  {
+            'host': 'localhost',
+            'port': PORT,
+            'database': DB_NAME,
+            'user': USER,
+            'password': PASS
+            }
         
     def pgConnection(self): 
         return psycopg2.connect(self.conn)
         
+
+conn = NewDBConnection().pgConnection
