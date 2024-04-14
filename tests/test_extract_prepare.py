c8bdb53e7c3c92c7a3f667d9912a0536790dae9b
ivbarauna@macbook <ivanildo.jnr@outlook.com>
2024-04-14 20:37:39 -0300

    feat: new test with valid type params but not valid avaliable params

--- tests/test_extract_prepare.py
+++ tests/test_extract_prepare.py
@@ -21,3 +21,7 @@ def test_pipelines_params_with_one_invalid_params():
 def test_pipelines_params_with_all_invalid_params():
     with pytest.raises(TypeError):
         ExecutePipeline(1,1,1)
+        
+def test_pipelines_params_with_all_invalid_keys_params():
+    with pytest.raises(KeyError):
+        ExecutePipeline("INVALID-QUOTE", "INVALID-QUOTE2")
