de4f6a48a4da917ecc37d47fa992b34276750298
IvanildoBarauna <ivanildo.jnr@outlook.com>
2024-05-21 12:27:47 -0300

    refactor: change path to test

--- tests/test_exceptions_pre_extract.py
+++ tests/test_exceptions_pre_extract.py
@@ -1,24 +1,30 @@
 import os
-import sys 
+import sys
+
 sys.path.append(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 import pytest
-from etl import ExecutePipeline
+from etl.controller.pipeline import ExecutePipeline
+
 
 def test_pipelines_params_with_unique_param():
     ExecutePipeline("USD-BRL")
-    
+
+
 def test_pipelines_params_with_two_param():
     ExecutePipeline("USD-BRL", "USD-BRLT")
-    
+
+
 def test_pipelines_params_with_one_invalid_params():
     with pytest.raises(expected_exception=TypeError):
         ExecutePipeline(1)
-        
+
+
 def test_pipelines_params_with_all_invalid_params():
     with pytest.raises(TypeError):
-        ExecutePipeline(1,1,1)
-        
+        ExecutePipeline(1, 1, 1)
+
+
 def test_pipelines_params_with_all_invalid_keys_params():
     with pytest.raises(KeyError):
         ExecutePipeline("INVALID-QUOTE", "INVALID-QUOTE2")
