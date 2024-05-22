6e2b31b295049e9bd6b355cc5503fe4c5d0a823b
IvanildoBarauna <ivanildo.jnr@outlook.com>
2024-05-22 11:43:22 -0300

    refactor: Update extraction and transformation tests
    This commit updates the extraction and transformation tests to reflect the changes made in the code. It adds new test cases for the extraction and transformation processes, ensuring that they handle different scenarios correctly. The tests now cover both success and failure cases, providing comprehensive test coverage for the code changes.

--- tests/test_ApiToParquetFile.py
+++ tests/test_ApiToParquetFile.py
@@ -1,20 +1,23 @@
-# import pytest
-# from etl.models.extract.ApiToParquetFile import extraction
+import pytest
+from etl.models.extract.ApiToParquetFile import extraction
 
-# def test_extraction_init():
-#     params = ["param1", "param2", "param3"]
-#     ext = extraction(params)
-#     assert ext.ValidParams == params
 
-# def test_extraction_run_success():
-#     params = ["param1", "param2", "param3"]
-#     ext = extraction(params)
-#     json_data = ext.__run__(ext.ValidParams)
-#     assert isinstance(json_data, dict)
+def test_extraction_init():
+    params = ["USD-BRL", "USD-BRLT", "CAD-BRL"]
+    ext = extraction(params)
+    assert ext.ValidParams == params
 
-# def test_extraction_run_failure():
-#     params = ["param1", "param2", "param3"]
-#     ext = extraction(params)
-#     ext.ValidParams = []
-#     json_data = ext.__run__(ext.ValidParams)
-#     assert json_data == {}
+
+def test_extraction_run_success():
+    params = ["USD-BRL", "USD-BRLT", "CAD-BRL"]
+    ext = extraction(params)
+    json_data = ext.__run__(ext.ValidParams)
+    assert isinstance(json_data, dict)
+
+
+def test_extraction_run_failure():
+    try:
+        params = []
+        ext = extraction(params)
+    except Exception:
+        assert pytest.raises(KeyError)
--- tests/test_reponseSplit.py
+++ tests/test_reponseSplit.py
@@ -1,33 +1,50 @@
-# import pytest
-# from etl.models.transform.ResponseSplit import transformation
+import pytest
+from etl.models.transform.ResponseSplit import transformation
 
-# @pytest.fixture
-# def json_response():
-#     return {
-#         "param1": {"key1": "value1"},
-#         "param2": {"key2": "value2"},
-#         "param3": {"key3": "value3"}
-#     }
 
-# @pytest.fixture
-# def valid_params():
-#     return ["param1", "param2", "param3"]
+@pytest.fixture
+def json_response() -> dict:
+    return {
+        "BRLCOP": {
+            "code": "BRL",
+            "codein": "COP",
+            "name": "Real Brasileiro/Peso Colombiano",
+            "high": "746.23",
+            "low": "741.33",
+            "varBid": "-8.36",
+            "pctChange": "-1.11",
+            "bid": "742.44",
+            "ask": "743.12",
+            "timestamp": "1716386175",
+            "create_date": "2024-05-22 10:56:15",
+        },
+        "BRLPEN": {
+            "code": "BRL",
+            "codein": "PEN",
+            "name": "Real Brasileiro/Sol do Peru",
+            "high": "0.7319",
+            "low": "0.7253",
+            "varBid": "-0.0098",
+            "pctChange": "-1.33",
+            "bid": "0.7116",
+            "ask": "0.7403",
+            "timestamp": "1716386118",
+            "create_date": "2024-05-22 10:55:18",
+        },
+    }
 
-# def test_transformation_process_param(json_response, valid_params):
-#     # Create an instance of the transformation class
-#     transform = transformation(json_response, valid_params)
 
-#     # Assert that the extracted_files list is populated correctly
-#     assert transform.extracted_files == [
-#         "/path/to/output/param1-20220101.parquet",
-#         "/path/to/output/param2-20220101.parquet",
-#         "/path/to/output/param3-20220101.parquet"
-#     ]
+@pytest.fixture
+def valid_params() -> list:
+    return ["BRL-COP", "BRL-PEN"]
 
-#     # Assert that the number of extracted files is correct
-#     assert len(transform.extracted_files) == 3
 
-#     # Assert that the files are written in the correct format
-#     assert transform.extracted_files[0].endswith(".parquet")
-#     assert transform.extracted_files[1].endswith(".parquet")
-#     assert transform.extracted_files[2].endswith(".parquet")
+def test_transformation_process_param(json_response, valid_params):
+    # Create an instance of the transformation class
+    transform = transformation(json_response, valid_params)
+
+    assert len(transform.extracted_files) == 2
+
+    for extracted_file in transform.extracted_files:
+        assert extracted_file.startswith("BRL-")
+        assert extracted_file.endswith(".parquet")
