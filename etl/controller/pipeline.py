b6352a6117b65a9761c85db3751393b7c3dfe612
IvanildoBarauna <ivanildo.jnr@outlook.com>
2024-05-22 17:34:46 -0300

    feat: Added Queue from processing and Load Data with paralelism

--- etl/controller/pipeline.py
+++ etl/controller/pipeline.py
@@ -1,5 +1,11 @@
+import threading
+import queue
+
 from etl.models.extract.ApiToParquetFile import extraction
 from etl.models.transform.ResponseSplit import transformation
+from etl.models.load.ToParquet import loadToParquet
+
+fila_unica = queue.Queue()
 
 
 class ExecutePipeline:
@@ -35,15 +41,48 @@ class ExecutePipeline:
         if totalInvalidParams == self.params_count:
             raise TypeError(f"Invalid parameters >>>> {self.params}")
 
-        self.__pipelineExecute__(InputParams=self.params)
+        self._pipeline_execute(InputParams=self.params)
 
-    def __pipelineExecute__(self, InputParams: list):
+    def _pipeline_execute(self, InputParams: list):
         """
         Executes the pipeline.
 
         Raises:
             KeyError: If the informed parameters are not available for extraction.
-
         """
-        NewExt = extraction(InputParams)
-        NewTransform = transformation(NewExt.json_data, NewExt.ValidParams)
+        try:
+            extractor = extraction(InputParams)
+
+            # Define a função que será executada pelo thread do produtor
+            def produce():
+                transformer = transformation(
+                    extractor.json_data, extractor.ValidParams, fila_unica
+                )
+                transformer.publish()
+                fila_unica.put(None)  # Sinaliza que a produção está completa
+
+            # Define a função que será executada pelo thread do consumidor
+            def consume():
+                while True:
+                    item = fila_unica.get()
+                    if item is None:
+                        fila_unica.task_done()
+                        break
+                    loader = loadToParquet(item)
+                    loader.load()
+                    fila_unica.task_done()
+
+            # Criação dos threads
+            thread_producer = threading.Thread(target=produce)
+            thread_consumer = threading.Thread(target=consume)
+
+            # Inicia os threads
+            thread_producer.start()
+            thread_consumer.start()
+
+            fila_unica.join()
+
+        except Exception as e:
+            # Tratamento genérico para outras exceções
+            print(f"Erro durante a execução do pipeline: {e}")
+            raise e
