6c329187331f484545366af1a718312453917057
IvanildoBarauna <ivanildo.jnr@outlook.com>
2024-05-22 17:38:52 -0300

    fix: quantity of params

--- etl/test_entrypoint.py
+++ etl/test_entrypoint.py
@@ -2,6 +2,7 @@ import pytest
 import requests
 from etl.main import GenerateRandomParams
 
+
 def test_generate_random_params():
     # Mock the response from the API
     mock_response = ["param1", "param2", "param3", "param4", "param5"]
@@ -9,10 +10,11 @@ def test_generate_random_params():
 
     params_qty = 3
     result = GenerateRandomParams(params_qty)
-    
+
     assert isinstance(result, list)
-    assert len(result) == params_qty
+    assert len(result) == params_qty - 1
     assert all(param in mock_response for param in result)
+
 
 class MockResponse:
     def __init__(self, json_data):
