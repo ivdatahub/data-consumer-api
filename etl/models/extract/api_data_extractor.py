951878781b7cf23ebc02289643b688905c1f5e17
IvanildoBarauna <ivanildo.jnr@outlook.com>
2024-05-24 16:47:32 -0300

    refactor: Construtor API Data Extractor

--- etl/models/extract/api_data_extractor.py
+++ etl/models/extract/api_data_extractor.py
@@ -1,77 +1,61 @@
 # Imports de Bibliotecas Padrão
 import time
+from typing import Tuple
 
 # Imports de Bibliotecas de Terceiros
 import requests
 from tqdm import tqdm
 
 # Imports de Módulos Internos
-from etl.common.utils.logs import loggingInfo, loggingWarn
-from etl.config.logFile import logFileName
+from etl.common.utils.logs import logging_info, logging_warn
+from etl.config.logFile import log_file_name
 from etl.config.datasource import API
 from . import params_validator as Validation
 
-WORK_DIR = logFileName(file=__file__)
+dir = log_file_name(file=__file__)
 
 
 class extraction:
     def __init__(self, params: list) -> None:
-        """
-        Initializes the extraction class.
-
-        Args:
-            ValidParams (list): A list of valid parameters.
-
-        Returns:
-            None
-        """
         self.params = params
-        __validator__ = Validation.ParamsValidator(params)
-        self.ValidParams = __validator__.ValidParamsForCall()
-        self.json_data = self.__run__(self.ValidParams)
 
-    def __run__(self, ValidParams: list) -> dict:
-        """
-        Runs the data extraction pipeline.
+    def run(self) -> Tuple[dict, list]:
+        validator = Validation.ParamsValidator(self.params)
+        valid_params = validator.valid_params_for_call()
 
-        Returns:
-            list: A list of extracted file paths.
-        """
-        ## extract Data
-        maked_endpoint = API.ENDPOINT_LAST_COTATION + ",".join(ValidParams)
-        loggingInfo(
+        maked_endpoint = API.ENDPOINT_LAST_COTATION + ",".join(valid_params)
+        logging_info(
             f"Sending request to: {API.ENDPOINT_LAST_COTATION} :: 1 of {API.RETRY_ATTEMPTS}",
-            WORK_DIR,
+            dir,
         )
         response = requests.get(maked_endpoint)
 
-        for tryNumber in range(API.RETRY_ATTEMPTS):
+        for try_number in range(API.RETRY_ATTEMPTS):
             if response.ok:
-                loggingInfo(
-                    f"Request finished with status {response.status_code}", WORK_DIR
+                logging_info(
+                    f"Request finished with status {response.status_code}", dir
                 )
                 json_data = response.json()
-                return json_data
+                return json_data, valid_params
             else:
-                if tryNumber < API.RETRY_ATTEMPTS - 1:
-                    loggingWarn(
+                if try_number < API.RETRY_ATTEMPTS - 1:
+                    logging_warn(
                         f"""response error, status_code {response.status_code}. 
                         Retrying in {API.RETRY_TIME_SECONDS} seconds...""",
-                        WORK_DIR,
+                        dir,
                     )
                     for _ in tqdm(range(100), total=100, desc=f"loading"):
                         time.sleep(API.RETRY_TIME_SECONDS / 100)
-                    loggingInfo(
-                        f"Sending request to: {API.ENDPOINT_LAST_COTATION} :: {tryNumber + 2} of {API.RETRY_ATTEMPTS}",
-                        WORK_DIR,
+                    logging_info(
+                        f"Sending request to: {API.ENDPOINT_LAST_COTATION} :: {try_number + 2} of {API.RETRY_ATTEMPTS}",
+                        dir,
                     )
                 else:
-                    loggingWarn("Attempt limits exceeded", WORK_DIR)
+                    logging_warn("Attempt limits exceeded", dir)
                     raise ConnectionError(
                         f"""Could not connect to the server after 3 attempts. 
                         Please try again later. 
                         Response status code: {response.status_code}"""
                     )
-        return (
-            {}
-        )  # Add this line to return an empty dictionary if no other return statement is reached
+
+        return ({}, [])
