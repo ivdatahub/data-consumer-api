86c26a383f95a3537c51b64785d99615cd52a4f1
IvanildoBarauna <ivanildo.jnr@outlook.com>
2024-05-28 17:42:48 -0300

    feat: PEP8 Conventions

--- etl/run.py
+++ etl/run.py
@@ -1,4 +1,5 @@
 import time
+
 import sys
 import random
 
@@ -20,29 +21,27 @@ def generate_random_params(params_qty: int) -> list:
     Returns:
         list: A list of randomly generated parameters.
     """
-    avaliable_list = list(requests.get(API.ENDPOINT_AVALIABLE_PARITIES).json())
-    min = random.randint(0, len(avaliable_list) - params_qty)
-    max = min + params_qty
-    if max > len(avaliable_list):
-        max = len(avaliable_list)
-    if params_qty == len(avaliable_list):
-        max -= max
-    return avaliable_list[min : max - 1]
+    available_list = list(requests.get(API.ENDPOINT_AVALIABLE_PARITIES).json())
+    first_number = random.randint(0, len(available_list) - params_qty)
+    last_number = first_number + params_qty
+    if last_number > len(available_list):
+        last_number = len(available_list)
+    if params_qty == len(available_list):
+        last_number -= last_number
+    return available_list[first_number:last_number - 1]
 
 
 def main(total_files: int = 2):
-    NewExec = PipelineExecutor(*generate_random_params(total_files))
-    NewExec.pipeline_run()
+    new_exec = PipelineExecutor(*generate_random_params(total_files))
+    new_exec.pipeline_run()
 
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        # Pega o número de parâmetros da linha de comando
         params_count = int(sys.argv[1])
     else:
-        # Gera um número aleatório de parâmetros
         params_count = random.randint(3, 20)
 
     main(params_count)
 
-print("Tempo decorrido: ", round(time.time() - start, 2), "segundos")
+print("Elapsed Time: ", round(time.time() - start, 2), "seconds")
