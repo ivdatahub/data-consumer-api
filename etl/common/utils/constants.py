01138bcddcd13874ad3a625d392c19cb4ddc4c84
Ivanildo Barauna <61716115+IvanildoBarauna@users.noreply.github.com>
2024-06-01 16:25:51 +0000

    chore: remove constants

--- etl/common/utils/constants.py
+++
@@ -1,5 +0,0 @@
-ENDPOINT_LIST_AVALIABLE_PARITYS = "https://economia.awesomeapi.com.br/json/available"
-"""str: The endpoint URL for retrieving the list of available parities."""
-
-ENDPOINT_QUOTES_AWESOME_API = "https://economia.awesomeapi.com.br/last/"
-"""str: The endpoint URL for retrieving the latest quotes from the Awesome API."""
