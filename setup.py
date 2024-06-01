535521d422a79b9113b08cdddc6199d4c5e24bdf
ivbarauna <ivanildo.jnr@outlook.com>
2024-06-01 00:56:12 -0300

    test

--- setup.py
+++ setup.py
@@ -1,12 +1,10 @@
 from setuptools import setup, find_packages
 
-
 def parse_requirements(filename):
     with open(filename, "r") as f:
         lines = f.read().splitlines()
         requirements = [line for line in lines if line and not line.startswith("#")]
     return requirements
-
 
 setup(
     name='"ETL-awesome-api"',
