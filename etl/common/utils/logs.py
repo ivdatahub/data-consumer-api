8a20a947c10574f718d60de66c708686d6a0784a
Ivanildo Barauna <61716115+IvanildoBarauna@users.noreply.github.com>
2024-06-01 16:26:26 +0000

    refactor: log components

--- etl/common/utils/logs.py
+++ etl/common/utils/logs.py
@@ -1,98 +1,95 @@
 import logging
 import os
 
-## fix-FolderLogBug
 
-LOG_FORMAT = "%(asctime)s :: %(levelname)s :: %(message)s"
+class CustomLogger():
+    def __init__(self, module) -> None:
+        self.module = module
+        self._log_format = "%(asctime)s :: %(levelname)s :: %(message)s"
+    
+    def _make_file_log(self):
+        dir_name = "../logs/"
+        os.makedirs(dir_name, exist_ok=True)
+        
+        with open(dir_name + f"{self.module}.log", "w") as f:
+            f.write("")
+            
+        return dir_name + f"{self.module}.log"
+    
+    
+    def _logger(self):
+        file_log = self._make_file_log()
+            
+        logging.basicConfig(
+            filename=file_log,
+            level=logging.DEBUG,
+            format=self._log_format,
+            datefmt="%Y-%m-%d %H:%M:%S",
+            )
+        
+        consoleLog = logging.getLogger("consoleLogger")
+        
+        consoleLog.setLevel(logging.INFO)
+        ch = logging.StreamHandler()
+        ch.setLevel(logging.INFO)
+        formatter = logging.Formatter(self._log_format)
+        ch.setFormatter(formatter)
 
+        consoleLog.addHandler(ch)
 
-def ConsoleLogger(module):
-    """
-    Creates a console logger for logging messages to the console and a log file.
-
-    Args:
-        module (str): The name of the module.
-
-    Returns:
-        logging.Logger: The console logger instance.
-    """
-    dir_name = f"../logs/"
-    os.makedirs(dir_name, exist_ok=True)
-
-    with open(dir_name + f"{module}.log", "w") as f:
-        f.write("")
-
-    logging.basicConfig(
-        filename=dir_name + f"{module}.log",
-        level=logging.DEBUG,
-        format=LOG_FORMAT,
-        datefmt="%Y-%m-%d %H:%M:%S",
-    )
-
-    consoleLog = logging.getLogger("consoleLogger")
-    consoleLog.setLevel(logging.INFO)
-    ch = logging.StreamHandler()
-    ch.setLevel(logging.INFO)
-    formatter = logging.Formatter(LOG_FORMAT)
-    ch.setFormatter(formatter)
-
-    consoleLog.addHandler(ch)
-
-    return consoleLog
-
-
-def logging_info(msg, module):
-    """
-    Logs an informational message.
-
-    Args:
-        msg (str): The message to be logged.
-        module (str): The name of the module.
-
-    Returns:
-        None
-    """
-    if logging.getLogger("consoleLogger").hasHandlers():
-        logger = logging.getLogger("consoleLogger")
-    else:
-        logger = ConsoleLogger(module=module)
-
-    logger.info(msg=msg)
+        return consoleLog
 
 
-def logging_error(msg, module):
-    """
-    Logs an error message.
+    def info(self, msg: str):
+        """
+        Logs an informational message.
 
-    Args:
-        msg (str): The message to be logged.
-        module (str): The name of the module.
+        Args:
+            msg (str): The message to be logged.
+            module (str): The name of the module.
 
-    Returns:
-        None
-    """
-    if logging.getLogger("consoleLogger").hasHandlers():
-        logger = logging.getLogger("consoleLogger")
-    else:
-        logger = ConsoleLogger(module=module)
+        Returns:
+            None
+        """
+        if logging.getLogger("consoleLogger").hasHandlers():
+            logger = logging.getLogger("consoleLogger")
+        else:
+            logger = CustomLogger(module=self.module)._logger()
 
-    logger.error(msg=msg)
+        logger.info(msg=msg)
 
 
-def logging_warn(msg, module):
-    """
-    Logs a warning message.
+    def error(self, msg: str):
+        """
+        Logs an error message.
 
-    Args:
-        msg (str): The message to be logged.
-        module (str): The name of the module.
+        Args:
+            msg (str): The message to be logged.
+            module (str): The name of the module.
 
-    Returns:
-        None
-    """
-    if logging.getLogger("consoleLogger").hasHandlers():
-        logger = logging.getLogger("consoleLogger")
-    else:
-        logger = ConsoleLogger(module=module)
+        Returns:
+            None
+        """
+        if logging.getLogger("consoleLogger").hasHandlers():
+            logger = logging.getLogger("consoleLogger")
+        else:
+            logger = CustomLogger(module=self.module)._logger()
 
-    logger.warning(msg=msg)
+        logger.error(msg=msg)
+
+
+    def warning(self, msg: str):
+        """
+        Logs a warning message.
+
+        Args:
+            msg (str): The message to be logged.
+        Returns:
+            None
+        """
+        if logging.getLogger("consoleLogger").hasHandlers():
+            logger = logging.getLogger("consoleLogger")
+        else:
+            logger = CustomLogger(module=self.module)._logger()
+
+        logger.warn(msg=msg)
