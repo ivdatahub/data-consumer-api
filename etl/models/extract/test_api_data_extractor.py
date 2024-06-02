a024fbdb697148c981bead4c7e68442e12f68803
ivbarauna <ivanildo.jnr@outlook.com>
2024-06-01 21:48:23 -0300

    tests: controler + api extractor

--- etl/models/extract/test_api_data_extractor.py
+++ etl/models/extract/test_api_data_extractor.py
@@ -1,15 +1,7 @@
 from etl.models.extract.api_data_extractor import extraction
 
 
-def test_extraction_init():
+def test_extraction_constructor():
     params = ["USD-BRL", "USD-BRLT", "CAD-BRL"]
     extractor = extraction(params)
-    response, valid_params = extractor.run
-    assert valid_params == params
-
-
-def test_extraction_run_success():
-    params = ["USD-BRL", "USD-BRLT", "CAD-BRL"]
-    extractor = extraction(params)
-    response, valid_params = extractor.run
-    assert isinstance(response, dict)
+    assert extractor.params == params
