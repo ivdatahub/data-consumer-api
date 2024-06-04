9cf21d0897cae4fadccf0a55cfabd533c4292833
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-06-04 16:52:40 -0300

    pragma: no cover

--- etl/run.py
+++ etl/run.py
@@ -11,8 +11,7 @@ def main(params: str = "USD-BRL"):
     new_exec.pipeline_run()
 
 
-if __name__ == "__main__":
-    # pragma: no cover
+if __name__ == "__main__":  # pragma: no cover
     if len(sys.argv) > 1:
         params = str(sys.argv[1])
         main(params)
