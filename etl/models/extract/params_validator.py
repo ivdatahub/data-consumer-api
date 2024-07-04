a7819faf13718e4ac48e8f5a9be53482250afcc5
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-07-03 22:13:42 -0300

    chore: Update Client builder

--- etl/models/extract/params_validator.py
+++ etl/models/extract/params_validator.py
@@ -20,9 +20,9 @@ class ParamsValidator:
 
         client = ClientBuilder(
             endpoint=API.ENDPOINT_AVALIABLE_PARITIES,
-            retry_strategy=RetryStrategies.LinearRetryStrategy,
+            retry_strategy=RetryStrategies.LINEAR_RETRY_STRATEGY,
             connection_timeout=API.CONNECTION_TIMEOUT,
-            delay=API.RETRY_TIME_SECONDS,
+            initial_delay=API.RETRY_TIME_SECONDS,
             retries=API.RETRY_ATTEMPTS,
         )
 
