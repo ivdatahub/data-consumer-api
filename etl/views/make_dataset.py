420b641d467fcfa30b3a445d48df68d26a180b04
Ivanildo Barauna de Souza Junior <ivanildo.jnr@outlook.com>
2024-07-24 14:10:46 -0300

    fix: Dockerfile image

--- etl/views/make_dataset.py
+++ etl/views/make_dataset.py
@@ -1,12 +1,13 @@
 import pandas as pd
 
-from etl.common.utils.common import default_output_folder as DefaultFolder
+from etl.common.utils.common import default_output_folder as DefaultFolder, dataset_path
 
 
 class DatasetSerializer:
     @staticmethod
     def serialize(files):
-        serialized_df = pd.read_pickle("etl/views/dataset.pkl")
+        ds = dataset_path()
+        serialized_df = pd.read_pickle(ds)
         print("Total rows before execution: ", serialized_df.shape[0])
         dfs = []
         for file in files:
@@ -17,6 +18,6 @@ class DatasetSerializer:
 
         new_df = pd.concat([delta_df, serialized_df], ignore_index=True)
 
-        pd.to_pickle(new_df, "etl/views/dataset.pkl")
+        pd.to_pickle(new_df, ds)
         print("Total rows after execution:: ", new_df.shape[0])
         print("New rows added: ", new_df.shape[0] - serialized_df.shape[0])
